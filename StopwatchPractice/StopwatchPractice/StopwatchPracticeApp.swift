257d4944749d9ffa01ea45f684a9c7661ef0784a
codeztree <codeztree@naver.com>
2023-07-12 01:08:25 +0900

    Initial Commit

---
+++ StopwatchPractice/StopwatchPractice/StopwatchPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchPracticeApp.swift
+//  StopwatchPractice
+//
+//  Created by 문창준 on 2023/07/08.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
